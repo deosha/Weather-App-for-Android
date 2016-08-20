007c6bd49da1a8f3e62d6afcfe849d447d581c2e
Deo Shankar <deoshankar89@gmail.com>
2016-08-20 21:39:56 +0530

    Updated the activity class

--- Weather/app/src/main/java/com/deoshankar/weather/WeatherActivity.java
+++ Weather/app/src/main/java/com/deoshankar/weather/WeatherActivity.java
@@ -1,13 +1,51 @@
 package com.deoshankar.weather;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+/**
+ * Created by deoshankar on 8/14/2016.
+ */
 
-public class WeatherActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+public class RemoteFetch {
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+
+    public static JSONObject getJSON(Context context, String city){
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+            connection.addRequestProperty("x-api-key",
+                    context.getString(R.string.open_weather_maps_app_id));
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
     }
 }
